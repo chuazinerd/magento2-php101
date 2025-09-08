4e251de608b9b0bd505f88fb8a8e58d0b3d643ef
Felipe Figueiredo <recoverxfj@gmail.com>
2025-09-08 10:18:33 -0300

    Create a constant in PHP to store a fixed value

--- criando-variaveis/criando-nomeando-variaveis.php
+++ criando-variaveis/criando-nomeando-variaveis.php
@@ -2,11 +2,15 @@
 $tittle = "Create an variable";
 $numPost = 10;
 $numPostDisplay = "\"$numPost\" posts";
-
+define('NEW_VALUE', 7);
+const NEW_VALUR = 21;
 ?>
 
 <h1><?= $tittle; ?></h1>
 
 <h2> <?= $numPostDisplay ;?></h2>
+
+<h3>Valor guardado: <?= NEW_VALUE ?> </h3>
+<h3>Novo valor: <?= NEW_VALUR ?> </h3>
 
 
