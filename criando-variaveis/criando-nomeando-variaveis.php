059e27b6d918f859abf8de470c1945c9119ee071
Felipe Figueiredo <recoverxfj@gmail.com>
2025-09-08 09:00:01 -0300

    PHP data type assignments

--- criando-variaveis/criando-nomeando-variaveis.php
+++ criando-variaveis/criando-nomeando-variaveis.php
@@ -1,3 +1,13 @@
-<?php   $tittle = "Create an variable";
+<?php
+$tittle = "Create an variable";
+$numPost = 10;
+?>
 
-echo $tittle;
+<h1><?= $tittle; ?></h1>
+
+<p> <?= $tittle ?> </p>
+<p ><?= gettype($tittle); ?> </p>
+<h2> <?= $numPost ;?></h2>
+<p><?= gettype($numPost) ; ?> </p>
+
+
