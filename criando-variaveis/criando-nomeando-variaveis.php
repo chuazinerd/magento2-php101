cec5b9a0dd3569f3178fbcec1de6b531c8965b4e
Felipe Figueiredo <recoverxfj@gmail.com>
2025-09-08 09:36:08 -0300

    Concatenating strings in PHP

--- criando-variaveis/criando-nomeando-variaveis.php
+++ criando-variaveis/criando-nomeando-variaveis.php
@@ -1,13 +1,12 @@
 <?php
-$tittle = (int) "Create an variable";
-$numPost = "10" . "10";
+$tittle = "Create an variable";
+$numPost = 10;
+$numPostDisplay = $numPost . ' posts';
+
 ?>
 
 <h1><?= $tittle; ?></h1>
 
-<p> <?= $tittle ?> </p>
-<p ><?= gettype($tittle); ?> </p>
-<h2> <?= $numPost ;?></h2>
-<p><?= gettype($numPost) ; ?> </p>
+<h2> <?= $numPostDisplay ;?></h2>
 
 
