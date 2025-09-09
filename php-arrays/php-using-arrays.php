e9ea51482b9d5c2204265b137891cbe31a7bfaa0
Felipe Figueiredo <recoverxfj@gmail.com>
2025-09-09 07:05:52 -0300

    Use an array key to determine current iteration

--- php-arrays/php-using-arrays.php
+++ php-arrays/php-using-arrays.php
@@ -3,7 +3,12 @@ $title = 'My Blog';
 $numPosts = 10;
 $hasPosts = $numPosts > 0;
 $numPostsDisplay = "\"$numPosts\" posts";
-$tags = ['php', 'mysql', 'docker'];
+$tags = ['php', 'docker', 'mysql'];
 ?>
-<h1><?=$title?></h1>
-<h2><?=$numPostsDisplay?></h2>
+    <h1><?= $title ?></h1>
+    <h2><?= $numPostsDisplay ?></h2>
+<?php
+foreach ($tags as $key => $tag) {
+    echo "$key: $tag<br>";
+}
+?>
