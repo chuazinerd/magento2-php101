6a1416965c52f1fd153197b7da2be4cb0b03c43c
Felipe Figueiredo <recoverxfj@gmail.com>
2025-09-08 23:14:35 -0300

    Refactor a PHP if else statement into a ternary

---
+++ condicionais/ternary-operator.php
@@ -0,0 +1,10 @@
+<?php
+
+$title = 'My Blog';
+$numPosts = 10;
+$hasPosts = $numPosts > 0;
+$numPostsDisplay = "\"$numPosts\" posts";
+?>
+    <h1><?= $title ?></h1>
+    <h2><?= $numPostsDisplay ?></h2>
+<?= $hasPosts ? 'Posts exist' : 'There are no posts.'?>
