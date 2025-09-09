5803a8f812848b314b88b0d360692a071b5382cf
Felipe Figueiredo <recoverxfj@gmail.com>
2025-09-08 23:25:48 -0300

    Use a match statement for advanced PHP data checks

---
+++ condicionais/advanced-data-checks.php
@@ -0,0 +1,14 @@
+<?php
+$title = 'My Blog';
+$numPosts = 10;
+$hasPosts = $numPosts > 0;
+$numPostsDisplay = "\"$numPosts\" posts";
+$message =match($numPosts) {
+    0 => 'There are no posts.',
+    1, 2, 3 => 'There are a few posts.',
+    default => 'There are many posts.',
+};
+?>
+<h1><?=$title?></h1>
+<h2><?=$numPostsDisplay?></h2>
+<p><?=$message?></p>
